dcd02580554a0e64a9e282c2a3be7b7891b226cf
carlos <skarlos134@gmail.com>
2023-11-26 15:50:37 -0500

    la funcionalidad para calcular el precio de los stickers funciona

--- calcular-precios.js
+++ calcular-precios.js
@@ -1,12 +1,54 @@
-let ancho = 4;
-let alto = 4;
+let ancho;
+let alto;
 let comision = 6.25;
-let cantidades = [50, 100, 300, 500, 1000];
+let cantidades = [50, 100, 200, 300, 500, 1000];
 
 function calcularArea() {
   return ancho * alto;
 }
+//////////////////////////////////////////////////
+let seleccionTamano = document.querySelector("#tamano");
 
+function seleccionTamanos() {
+  let valorSeleccionado = seleccionTamano.value;
+  if (valorSeleccionado == "1") {
+    ancho = 3;
+    alto = 3;
+    correr();
+  } else if (valorSeleccionado == "2") {
+    ancho = 4;
+    alto = 4;
+    correr();
+  } else if (valorSeleccionado == "3") {
+    ancho = 5;
+    alto = 5;
+    correr();
+  } else if (valorSeleccionado == "4") {
+    ancho = 6;
+    alto = 6;
+    correr();
+  } else if (valorSeleccionado == "5") {
+    let tBody = document.querySelector("tbody");
+    tBody.innerText = "";
+  }
+}
+
+let btnCalcularPrecio = document.querySelector("#btnCalcularPrecio");
+
+btnCalcularPrecio.addEventListener("click", function (event) {
+  event.preventDefault(); // Evita la acción predeterminada del botón (submit)
+  calcularPrecioTamanoPersonalizado();
+});
+
+function calcularPrecioTamanoPersonalizado() {
+  ancho = parseFloat(document.querySelector("#ancho").value);
+  alto = parseFloat(document.querySelector("#alto").value);
+  console.log("Valores de ancho y alto:", ancho, alto);
+  correr();
+}
+
+seleccionTamano.addEventListener("change", seleccionTamanos);
+//////////////////////////////////////////////////
 function calcularPrecio() {
   let preciosCantidad = [];
   cantidades.forEach((cantidad) => {
